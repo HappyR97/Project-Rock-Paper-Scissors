1628bef039bd47b4d2abd656c416c512c5613f4f
Rida Berkaoui <ridaberkaoui1997@gmail.com>
2022-12-29 11:20:26 +0100

    Create loop that plays 5 rounds of the game and keeps track of the score

--- script.js
+++ script.js
@@ -11,29 +11,47 @@ function getComputerChoice() {
   }
 }
 
-getComputerChoice();
+let score = 0;
 
-const computerSelection = getComputerChoice();
-const playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
-
-const playRound = function (playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
   if (playerSelection === "rock" && computerSelection === "paper") {
     return "You lose! Paper beats Rock";
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    score++;
     return "You win! Rock beats Scissors";
   } else if (playerSelection === "paper" && computerSelection === "rock") {
+    score++;
     return "You win! Paper beats Rock";
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
     return "You lose! Scissors beat Paper";
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
     return "You lose! Rock beats scissors";
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    score++;
     return "You win! Scissors beat Rock";
   } else if (playerSelection === computerSelection) {
     return "It's a draw!";
   } else {
     alert("You didn't choose a valid option");
   }
+}
+
+const game = function () {
+  for (let i = 0; i < 5; i++) {
+    getComputerChoice();
+    let computerSelection = getComputerChoice();
+    console.log(computerSelection);
+
+    let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
+
+    playRound(playerSelection, computerSelection);
+    console.log(score);
+  }
+  if (score >= 3) {
+    return "You win the game";
+  } else {
+    return "You lose the game.";
+  }
 };
 
-console.log(playRound(playerSelection, computerSelection));
+game();
