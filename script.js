8bb371d4202603f048674d83825f03aebe31f2da
Rida Berkaoui <ridaberkaoui1997@gmail.com>
2023-01-01 14:14:45 +0100

    Add HTML elements to select buttons and display results Make EventListener for all buttons Configure EventListener for Rock as a starter:  - Manipulate DOM according to results  - Keep and display scores for both player and computer

--- script.js
+++ script.js
@@ -11,23 +11,27 @@ function getComputerChoice() {
   }
 }
 
-let score = 0;
+let playerScore = 0;
+let computerScore = 0;
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === "rock" && computerSelection === "paper") {
+    computerScore++;
     return "You lose! Paper beats Rock";
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    score++;
+    playerScore++;
     return "You win! Rock beats Scissors";
   } else if (playerSelection === "paper" && computerSelection === "rock") {
-    score++;
+    playerScore++;
     return "You win! Paper beats Rock";
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    computerScore++;
     return "You lose! Scissors beat Paper";
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    computerScore++;
     return "You lose! Rock beats scissors";
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    score++;
+    playerScore++;
     return "You win! Scissors beat Rock";
   } else if (playerSelection === computerSelection) {
     return "It's a draw!";
@@ -36,22 +40,44 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-const game = function () {
-  for (let i = 0; i < 5; i++) {
-    getComputerChoice();
-    let computerSelection = getComputerChoice();
-    console.log(computerSelection);
+// const computerSelection = getComputerChoice();
+// console.log(computerSelection);
 
-    let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
+const btnRock = document.querySelector(".btn--rock");
+const btnPaper = document.querySelector(".btn--paper");
+const btnScissors = document.querySelector(".btn--scissors");
+const playerScoreDOM = document.querySelector(".playerScore");
+const cpuChoice = document.querySelector(".cpuChoice");
+const computerScoreDOM = document.querySelector(".computerScore");
+const roundResult = document.querySelector(".roundResult");
 
-    playRound(playerSelection, computerSelection);
-    console.log(score);
-  }
-  if (score >= 3) {
-    return "You win the game";
-  } else {
-    return "You lose the game.";
-  }
-};
+btnRock.addEventListener("click", function () {
+  let computerSelection = getComputerChoice();
+  cpuChoice.textContent = `Computer chose: ${computerSelection}`;
 
-console.log(game());
+  roundResult.textContent = playRound("rock", computerSelection);
+  playerScoreDOM.textContent = `Your score: ${playerScore}`;
+  computerScoreDOM.textContent = `Computer score: ${computerScore}`;
+});
+// btnPaper.addEventListener("click", playRound("paper", computerSelection));
+// btnScissors.addEventListener("click", playRound("scissors", computerSelection));
+// Play 5 rounds
+// const game = function () {
+//   for (let i = 0; i < 5; i++) {
+//     getComputerChoice();
+//     let computerSelection = getComputerChoice();
+//     console.log(computerSelection);
+
+//     let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
+
+//     playRound(playerSelection, computerSelection);
+//     console.log(score);
+//   }
+//   if (score >= 3) {
+//     return "You win the game";
+//   } else {
+//     return "You lose the game.";
+//   }
+// };
+
+// console.log(game());
